9c467928318d7668c1475dfbdf2833e81ce7ff79
jerrywithaz <zerry.social@gmail.com>
2020-04-22 00:23:58 -0500

    file cleanup

--- client/src/views/ApplicationView/ApplicationView.style.tsx
+++ client/src/views/ApplicationView/ApplicationView.style.tsx
@@ -8,26 +8,3 @@ export const ApplicationView = styled.div`
 `;
 
 ApplicationView.displayName = "Styled.ApplicationView";
-
-export const Sidebar = styled.div`
-    width: 200px;
-    padding: 20px 20px;
-    background: #efefef;
-`;
-
-Sidebar.displayName = "Styled.Sidebar";
-
-export const Main = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-`;
-
-Main.displayName = "Styled.Main";
-
-export const Application = styled.div`
-    flex: 1;
-    padding: 20px 20px;
-`;
-
-Application.displayName = "Styled.Application";
